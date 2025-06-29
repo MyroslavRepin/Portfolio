e0aef5ecb9f080e04399c8ccae0d5aca6108269c
MyroslavRepin <myroslavrepin@gmail.com>
2025-06-28 21:03:13 -0600

    Refactor CSS for consistency and readability; update font imports and styles
    - Changed single quotes to double quotes for font-family and import URLs in base.css and layout.css.
    - Reformatted CSS properties for better readability by aligning them.
    - Standardized color codes to lowercase in layout.css.
    - Removed unnecessary comments and improved spacing in layout.css.
    - Ensured consistent use of font-family declarations across sections.
    - Updated JavaScript to improve readability by adjusting indentation in script.js.

--- static/js/script.js
+++ static/js/script.js
@@ -2,9 +2,9 @@ const fileInput = document.getElementById("file");
 const fileName = document.getElementById("file-name");
 
 fileInput.addEventListener("change", () => {
-    if (fileInput.files.length > 0) {
-      fileName.textContent = fileInput.files[0].name;
-    } else {
-      fileName.textContent = "No file chosen";
-    }
+  if (fileInput.files.length > 0) {
+    fileName.textContent = fileInput.files[0].name;
+  } else {
+    fileName.textContent = "No file chosen";
+  }
 });
